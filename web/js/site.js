1bcc4dde343839e4c71fd98fc3ca4861863e7c49
Mike Niebling <michael.niebling@gmail.com>
2015-05-01 16:26:13 -0500

    Use JS to open external links in _blank tab
    Trying this for now as I get more data about users’ expectations.

--- web/js/site.js
+++ web/js/site.js
@@ -5,7 +5,10 @@
 
 $(function () {
 
+	//
 	// Post images should have lightbox-zoom functionality
+	//
+	// -------------------------------------------------------
 	var $popupImages = $('.post img');
 
 	$popupImages.each(function () {
@@ -28,4 +31,12 @@ $(function () {
 	// image has been successfully enhanced
 	$popupImages.addClass('is-zoomable');
 
+
+	//
+	// External links should open in a new browser tab
+	//
+	// -------------------------------------------------------
+	$(document.links).filter(function() {
+    return this.hostname != window.location.hostname;
+	}).attr('target', '_blank');
 });
